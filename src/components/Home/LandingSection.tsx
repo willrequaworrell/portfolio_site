fcc79f9c4e03d5e216ddc86baa2ce139700d8d00
Will Worrell <willrequaworrell@gmail.com>
2025-03-07 17:21:39 -0500

    test

--- src/components/Home/LandingSection.tsx
+++ src/components/Home/LandingSection.tsx
@@ -51,10 +51,8 @@ const LandingSection = () => {
     }
 
     return (
-        <section id="landing" className="">
-            <div className="relative h-[100dvh]  bg-cover bg-center bg-red-50" 
-            // style={{backgroundImage: "url('https://i.ibb.co/cGxhbS4/thierry-meier-PKEPAe-MBOIU-unsplash.jpg')"}}
-            >
+        <section id="landing" >
+            <div className="relative min-h-[100dvh] max-h-screen bg-cover bg-center" style={{backgroundImage: "url('https://i.ibb.co/cGxhbS4/thierry-meier-PKEPAe-MBOIU-unsplash.jpg')"}}>
                 <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#052B30] opacity-100"></div>
                 <SocialLinkBubbleContainer>
                     <SocialLinkBubble icon={<FaLinkedin />} href="https://linkedin.com/in/wrw" openInNewTab/>
@@ -68,7 +66,7 @@ const LandingSection = () => {
                         variants={titleContainerVariants}
                         initial={"hidden"}
                         animate={"visible"}
-                        className="absolute bottom-0 left-0 mb-0 text-[#F4FFF0]"
+                        className="absolute bottom-1/10 sm:bottom-0 left-0 mb-0 text-[#F4FFF0]"
                     >
                         <motion.div 
                             className="flex items-baseline md:-my-[2vh] lg:-my-[4vh]"
