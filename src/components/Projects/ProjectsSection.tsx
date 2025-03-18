85ab606f71cf49e4d38c9d98e4a1ad66ada3bc93
Will Worrell <willrequaworrell@gmail.com>
2025-03-17 22:45:40 -0400

    Update project section layout on mobile

--- src/components/Projects/ProjectsSection.tsx
+++ src/components/Projects/ProjectsSection.tsx
@@ -55,9 +55,9 @@ const ProjectsSection = () => {
     return (
         <section className="font-ibm bg-gradient-to-b from-[#093941] to-[#052b32] overflow-x-clip">
             <div className="flex h-[100dvh] max-h-screen p-6 sm:p-8">
-                <div className="flex flex-grow flex-col justify-center gap-x-8 md:flex-row-reverse">
+                <div className="flex flex-grow flex-col justify-center gap-x-8 gap-y-4 md:flex-row-reverse">
                     <p className="text-start text-[10vw] md:text-[16vw] text-[#F4FFF0] font-climate md:hidden">Projects</p>
-                    <div className="flex h-full max-h-1/2 md:max-h-none w-full items-center md:h-auto md:w-1/2">
+                    <div className="flex h-1/3  md:max-h-none w-full items-center md:h-auto md:w-1/2">
                         <motion.div  
                             variants={contentVariants}
                             initial={"hidden"}
@@ -68,29 +68,30 @@ const ProjectsSection = () => {
                             <Carousel 
                                 currentSlide={currentProject}
                                 setCurrentSlide={setCurrentProject}
-                                slides={projectsData} />
+                                slides={projectsData} 
+                            />
                         </motion.div>
                     </div>
                     <motion.div 
                         variants={projectsVariants}
                         initial={"hidden"}
                         whileInView={"visible"}
-                        viewport={{ once: true }}
-                        className="flex md:w-1/2 py-4 md:py-0" 
+                        // viewport={{ once: true }}
+                        className="flex sm:h-full md:w-1/2 py-4  md:py-0" 
                     >
-                        <div className="flex w-full flex-col items-start text-[#F4FFF0]">
-                            <div className="flex items-end h-2/5">
+                        <div className="flex w-full flex-col items-start  text-[#F4FFF0]">
+                            <div className="flex items-end h-2/5 ">
                                 <p className="hidden text-[7vw] md:block font-climate">Projects</p>
                             </div>
                             <motion.div
                                 key={projectsData[currentProject].title}
                                 variants={descriptionVariants}
                                 initial={"hidden"}
-                                whileInView={"visible"}
+                                // whileInView={"visible"}
                                 animate={"visible"}
                             >
-                                <motion.p className="text-[4vw] md:text-[1.5vw] font-bold">{projectsData[currentProject].title}</motion.p>
-                                <motion.p className="text-[2.5vw] md:text-[1vw]">{projectsData[currentProject].description}</motion.p>
+                                <motion.p className="text-[3.5vw] md:text-[1.5vw] font-bold">{projectsData[currentProject].title}</motion.p>
+                                <motion.p className="text-[max(10px,2.5vw)] md:text-[1vw]">{projectsData[currentProject].description}</motion.p>
                             </motion.div>
                         </div>
                     </motion.div>
