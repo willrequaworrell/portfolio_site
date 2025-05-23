f86c1fd6cc11bc7ac769df6a610cc9239fd5866e
Will Worrell <willrequaworrell@gmail.com>
2025-05-23 13:28:43 -0400

    added er-404 and updated some other project data & images

--- src/util/ProjectsData.ts
+++ src/util/ProjectsData.ts
@@ -1,5 +1,13 @@
 export const projectsData = [
     {
+        title: "ER-404", 
+        description: "Inspired by legendary music hardware like the Roland TR-909, ER-404 is a modern take on the vintage drum machine. With fully customizable tracks, fx & playback, ER-404 is a gateway to endless rhythmic possibilities--all in the browser.", 
+        src: "./er404.png", 
+        alt: "ER-404 app logo", 
+        live: "https://er-404.com", 
+        github: "https://github.com/willrequaworrell/ER-404"
+    },
+    {
         title: "Klarr", 
         description: "A productivity dashboard designed to remove friction from daily task tracking with intuitive drag-and-drop functionality and minimalist Bauhaus-inspired styling. Born from frustration with clunky notes systems and overly complex apps, Klarr offers a visually compelling yet elegantly simple solution. Its tactile interface makes organizing tasks effortless, while customizable themes let users tailor the experience to their aesthetic preferences—ensuring it’s as enjoyable to use as it is functional.", 
         src: "./klarr.png", 
@@ -11,7 +19,7 @@ export const projectsData = [
         title: "BriteSpot", 
         description: "A simple, intuitive app designed to help users build a sustainable gratitude practice. By logging just one bright moment from your day, BriteSpot makes it easy to start small and stay consistent. The app tracks your journey toward a more gratitude-filled life, allowing you to view and sort past entries, export your logs, and even visualize patterns in your memories to discover what truly brings you joy. With BriteSpot, cultivating mindfulness and positivity becomes an achievable part of your daily routine.", 
         src: "./britespot.png", 
-        alt: "klarr app logo", 
+        alt: "BriteSpot app logo", 
         live: "https://britespot.vercel.app", 
         github: "https://github.com/willrequaworrell/brite-spot-frontend"
     }
